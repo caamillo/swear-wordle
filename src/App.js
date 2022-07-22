a6a0a823729a928dc238b2f5670266693748603d
camillo <diaskygotah@gmail.com>
2022-07-22 18:27:56 +0200

    init react + flex

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// Taiwind
+import './tailwind/input.css'
+import './tailwind/output.css'
+
+function App() {
+    return (
+        <div className="App">
+            <div className='wordle-navbar absolute w-full'>
+                <div className='flex items-center justify-between p-4 px-6'>
+                    <div>Menu</div>
+                    <div>Wordle</div>
+                    <div>Settings</div>
+                </div>
+            </div>
+            <div className='wordle-container flex justify-center items-center h-screen'>ciao</div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Components
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
